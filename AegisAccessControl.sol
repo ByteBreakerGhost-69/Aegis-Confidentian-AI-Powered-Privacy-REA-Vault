71ebcf1dc36d5cda47d87ca9c79e4846f552eebb
ByteBreakeGhost <yasyfinance.9@gmail.com>
2026-02-13 09:45:26 +0700

    Update AegisAccessControl.sol

--- AegisAccessControl.sol
+++ AegisAccessControl.sol
@@ -1,77 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-library AegisAccessControl {
-    struct AccessControlStorage {
-        mapping(address => bool) admins;
-        mapping(address => bool) subscriptions;
-        bool paused;
-        bool automationActive;
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract AegisAccessControl is AccessControl {
+    bytes32 public constant AI_ORACLE_ROLE = keccak256("AI_ORACLE_ROLE");
+    bytes32 public constant VAULT_MANAGER_ROLE = keccak256("VAULT_MANAGER_ROLE");
+    
+    bool private _paused;
+    
+    event Paused(address account);
+    event Unpaused(address account);
+    
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
     
-    bytes32 private constant STORAGE_SLOT = 
-        keccak256("aegis.access.control.storage");
-    
-    function getStorage() 
-        private 
-        pure 
-        returns (AccessControlStorage storage s) 
-    {
-        bytes32 slot = STORAGE_SLOT;
-        assembly {
-            s.slot := slot
-        }
+    modifier whenNotPaused() {
+        require(!_paused, "Contract paused");
+        _;
     }
     
-    // Admin functions
-    function isAdmin(address addr) internal view returns (bool) {
-        return getStorage().admins[addr];
+    function pause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _paused = true;
+        emit Paused(msg.sender);
     }
     
-    function addAdmin(address addr) internal {
-        getStorage().admins[addr] = true;
+    function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _paused = false;
+        emit Unpaused(msg.sender);
     }
     
-    function removeAdmin(address addr) internal {
-        getStorage().admins[addr] = false;
-    }
-    
-    // Subscription functions
-    function hasSubscription(address user) internal view returns (bool) {
-        return getStorage().subscriptions[user];
-    }
-    
-    function grantSubscription(address user) internal {
-        getStorage().subscriptions[user] = true;
-    }
-    
-    function revokeSubscription(address user) internal {
-        getStorage().subscriptions[user] = false;
-    }
-    
-    // Pause functions
-    function isPaused() internal view returns (bool) {
-        return getStorage().paused;
-    }
-    
-    function pause() internal {
-        getStorage().paused = true;
-    }
-    
-    function resume() internal {
-        getStorage().paused = false;
-    }
-    
-    // Automation functions
-    function isAutomationActive() internal view returns (bool) {
-        return getStorage().automationActive;
-    }
-    
-    function activateAutomation() internal {
-        getStorage().automationActive = true;
-    }
-    
-    function deactivateAutomation() internal {
-        getStorage().automationActive = false;
+    function isPaused() external view returns (bool) {
+        return _paused;
     }
 }
